96cf1ef827f43770e91bc31d8005e413a81416c7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-30 04:23:27 -0400

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -1,10 +1,12 @@
-using LinearAlgebra: norm
+import Base: angle
 
-function Base.angle(point1::T, point2::T) where {T<:Union{Tuple, Vector}}
-   norm1, norm2 = norm(point1), norm(point2)
-   norm1pt2 = norm1 .* (point2...,)
-   norm2pt1 = norm2 .* (point1...,)
-   x = norm(norm1pt2 .+ norm2pt1)
-   y = norm(norm1pt2 .- norm2pt1)
+using LinearAlgebra
+
+function Base.angle(point1::T, point2::T) where {N, R<:Real, T<:Union{NTuple{N,R}, AbstractArray{R,1}}}
+   rescaled_point1 = point1 .* LinearAlgebra.norm(point2)
+   rescaled_point2 = point2 .* LinearAlgebra.norm(point1)
+
+   x = LinearAlgebra.norm(rescaled_point2 .+ rescaled_point1)
+   y = LinearAlgebra.norm(rescaled_point2 .- rescaled_point1)
    return 2 * atan(y, x)
 end
