589bf9bc37032e79d0720fd7b72cad01d632ef31
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-05 21:06:06 -0400

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -9,8 +9,8 @@ import LinearAlgebra.norm
 
 # basic collection of types that may be used to represent points
 const InitialPointReprs =
-     Union{ NTuple{2, Float32}, NTuple{3, Float32}, NTuple{4, Floa32},
-            NTuple{2, Float64}, NTuple{3, Float64}, NTuple{4, Floa64},
+     Union{ NTuple{2, Float32}, NTuple{3, Float32}, NTuple{4, Float32},
+            NTuple{2, Float64}, NTuple{3, Float64}, NTuple{4, Float64},
             AbstractArray{Float32, 1}, AbstractArray{Float64, 1} }
 
 TypesForPoints = InitialPointReprs
@@ -25,14 +25,27 @@ Interoperable expansion of internal point representations.
 PointRepresentions( <your point representation types> }
 
 ```
+using LinearAlgebra
+using AngleBetweenPoints
+
 struct Point2D{T}
     x::T
     y::T
 end
 
-PointRepresentations(Point2D{Float32}, Point2D{Float64})
+LinearAlgebra.norm(pt::Point2D{T}) where {T} = sqrt(pt.x^2 + pt.y^2)
+
+Tuple(pt::Point2D{T}) = (pt.x, pt.y)
+
+PointRepresentation(Point2D{Float32}, Point2D{Float64})
+
+point1 = Point2D(0.0, 1.0)
+point2 = Point2D(1.0, 0.5)
+
+angle_between = angle(point1, point2)
+
 """
-function PointRepresentations(point_types...)
+function PointRepresentation(point_types...)
     global TypesForPoints
     added_point_types = Union{point_types...,}
     TypesForPoints = Union{TypesForPoints, added_point_types}
@@ -49,7 +62,7 @@ The angle is taken in the plane that contains both `apoint` and `bpoint`.
 
 If one of the points is at the origin, the result is undefined.
 """
-function Base.angle(point1::T, point2::T) where {N, R, T<:PointRepresentation{N, R}}
+function Base.angle(point1::T, point2::T) where {N,R,T<:NTuple{N,R}}
    rescaled_point1 = point1 .* norm(point2)
    rescaled_point2 = point2 .* norm(point1)
     
@@ -59,5 +72,6 @@ function Base.angle(point1::T, point2::T) where {N, R, T<:PointRepresentation{N,
    return 2 * atan(normalized_ordinate / normalized_abcissa)
 end
 
+Base.angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
 
 end # AngleBetweenVectors
