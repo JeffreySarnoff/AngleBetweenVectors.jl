f4aa03e0ca3e10a558bcbdfb5f8be1b8a41a26ff
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-05 20:24:03 -0400

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -21,14 +21,19 @@ PointRepr{N, R} = Union{NTuple{N, R}, AbstractArray{R, 1}}
 """
     angle( apoint::T, bpoint::T) where {N, R, T<:PointRepr{N,R}}
 
-accurately ascertains the undirected angle between to vectorial points
+Accurately ascertains the undirected angle (0 <= radians < pi)
+between two points given in Cartesian coordinates.
+
+The angle is taken in the plane that contains both `apoint` and `bpoint`.
+
+If one of the points is at the origin, the result is undefined.
 """
 function Base.angle(point1::T, point2::T) where {N, R, T<:PointRepresentation{N, R}}
    rescaled_point1 = point1 .* norm(point2)
    rescaled_point2 = point2 .* norm(point1)
-
-   x = norm(rescaled_point2 .+ rescaled_point1)
-   y = norm(rescaled_point2 .- rescaled_point1)
+    
+   normalized_abcissa  = norm(rescaled_point2 .+ rescaled_point1)
+   normalized_ordinate = norm(rescaled_point2 .- rescaled_point1)
    
-   return 2 * atan(y / x)
+   return 2 * atan(normalized_ordinate / normalized_abcissa)
 end
