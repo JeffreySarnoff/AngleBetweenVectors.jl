cf5c9241cd7020ff3104e499be7a2c9fbd50e9a1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 13:59:45 -0400

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -1,72 +1,22 @@
 module AngleBetweenVectors
 
-export angle, PointRepresentation!
+export angle
 
 import Base: angle
 
 import LinearAlgebra: norm, dot
 
-
-# basic collection of types that may be used to represent points
-const InitialPointReprs =
-     Union{ NTuple{2, Float32}, NTuple{3, Float32}, NTuple{4, Float32},
-            NTuple{2, Float64}, NTuple{3, Float64}, NTuple{4, Float64},
-            AbstractArray{Float32, 1}, AbstractArray{Float64, 1} }
-
-TypesForPoints = InitialPointReprs
-
-"""
-    PointRepresentation{types_representing_points...}
-
-Generate a `Union` of Point representations.
-
-Interoperable expansion of internal point representations.
-
-PointRepresention( <your point representation types> }
-
-```
-using AngleBetweenPoints
-
-struct Point2D{T}
-    x::T
-    y::T
-end
-
-#  always specialize the Tuple constructor
-Tuple(point::Point2D{T}) where {T} = (point.x, point.y)
-
-PointRepresentation!(Point2D{Float32}, Point2D{Float64})
-
-point1 = Point2D(0.0, 1.0)
-point2 = Point2D(1.0, 1.0)
-
-angle(point1, point2) / pi == 0.25
-true
-```
-"""
-function PointRepresentation!(point_types...)
-    global TypesForPoints
-    added_point_types = Union{point_types...,}
-    TypesForPoints = Union{TypesForPoints, added_point_types}
-    nothing
-end
-
-#=
-function kahanangle(p,b)
-    y = norm(p./norm(p) .- b./norm(b))
-    x = norm(p./norm(p) .+ b./norm(b))
-    2*atan(y,x)
-end
-=#
 """
     angle(point1::T, point2::T) where {T}
 
 Accurately ascertains the undirected angle (0 <= radians < pi)
-between two points given in Cartesian coordinates.
+between two points given in N-dimensional Cartesian coordinates.
 
 The angle is taken in the plane that contains both rays.
 
 If one of the points is at the origin, the result is zero.
+
+You *must* define a tuple constructor `Tuple(x::T) = ...`.
 """
 function Base.angle(point1::NT, point2::NT) where {N,F,NT<:NTuple{N,F}}
    unitvec1 = unitvec(point1)
