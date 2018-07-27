41a55f4f12c99cab8c7f135ac5140e1a5b2f8278
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 15:06:31 -0400

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -16,21 +16,19 @@ If one of the points is at the origin, the result is zero.
 
 You *must* define a tuple constructor `Tuple(x::T) = ...`.
 """
-function Base.angle(point1::NT, point2::NT) where {N,F,NT<:NTuple{N,F}}
+function angle(point1::NT, point2::NT) where {N,F,NT<:NTuple{N,F}}
    unitvec1 = unitvec(point1)
    unitvec2 = unitvec(point2)
     
    y = norm(unitvec1 .- unitvec2)
    x = norm(unitvec1 .+ unitvec2)
-   
-   !isfinite(x) || !isfinite(y) && throw(DomainError("finite points only"))
-       
+          
    a = (2 * atan(y, x))
    
-   zero(F) <= a < F(pi) ? a : zero(F)
+   zero(F) <= a <= F(pi) ? a : zero(F)
 end
 
-@inline Base.angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
+@inline angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
 
 @inline unitvec(p) = p ./ norm(p)
 
