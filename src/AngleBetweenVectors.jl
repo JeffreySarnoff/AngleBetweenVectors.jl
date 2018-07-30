28bdfd37a5701eff81481b7b12c4ef0bed234bfb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-29 22:23:30 -0400

    angle2 (a faster version)

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -1,34 +1,56 @@
 module AngleBetweenVectors
 
-export angle
+export angle, angle2
 
 import Base: angle
 
 import LinearAlgebra: norm, dot
 
+const PI = Float64(pi)
+
 """
-    angle(point1::T, point2::T) where {T}
+    angle2(point1::T, point2::T) where {T}
 
 Accurately ascertains the undirected angle (0 <= radians < pi)
-between two points given in N-dimensional Cartesian coordinates..
+between two points given in N-dimensional Cartesian coordinates.
+
+Prefer this to `angle` for highest performance. Works best
+where all |coordinates| are in 2^±12 or [1/4_000,4_000].
+Use `angle2` where |coordinates| may be outside 2^±20 or
+[1/1_000_000,1_000_000]. 
 
 If one of the points is at the origin, the result is zero.
 
 You *must* define a tuple constructor `Tuple(x::T) = ...`.
 """
-function angle(point1::NT, point2::NT) where {N,F<:AbstractFloat,NT<:NTuple{N,F}}
-    unitvec1 = unitvec(point1)
-    unitvec2 = unitvec(point2)
+function angle2(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
+    point1a = point1 .* norm(point2)
+    point2a = point2 .* norm(point1)
     
-    y = unitvec1 .- unitvec2
-    x = unitvec1 .+ unitvec2
-          
-    a = 2 * atan(norm(y) / norm(x))
-   
-    !signbit(a) && !(signbit(F(pi) - a)) ? a : (signbit(a) ? zero(F) : F(pi))
+    subpoints = point1a .- point2a
+    subpoints = dot(subpoints, subpoints)
+    addpoints = point1a .+ point2a
+    addpoints = dot(addpoints, addpoints)
+    
+    2 * atan(sqrt(subpoints ./ addpoints))
 end
 
-function angle(point1::NT, point2::NT) where {N,F<:Integer,NT<:NTuple{N,F}}
+"""
+    angle(point1::T, point2::T) where {T}
+
+Accurately ascertains the undirected angle (0 <= radians < pi)
+between two points given in N-dimensional Cartesian coordinates.
+
+Prefer this to `angle2` for highest accuracy.  Suggested
+when |coordinates| may be outside 2^±20 or [1/1_000_000,1_000_000].
+Strongly recommended when any |coordinates| are outside 2^±24 or
+[1/16_000_000,16_000_000].
+
+If one of the points is at the origin, the result is zero.
+
+You *must* define a tuple constructor `Tuple(x::T) = ...`.
+"""
+function angle(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
     unitvec1 = unitvec(point1)
     unitvec2 = unitvec(point2)
     
@@ -37,10 +59,11 @@ function angle(point1::NT, point2::NT) where {N,F<:Integer,NT<:NTuple{N,F}}
           
     a = 2 * atan(norm(y) / norm(x))
    
-    !signbit(a) && !(signbit(Float64(pi) - a)) ? a : (signbit(a) ? zero(F) : Float64(pi))
+    !signbit(a) && !(signbit(PI - a)) ? a : (signbit(a) ? zero(F) : PI)
 end
 
 @inline angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
+@inline angle2(point1::T, point2::T) where {T} = angle2(Tuple(point1), Tuple(point2))
 
 @inline unitvec(p) = p ./ norm(p)
 
