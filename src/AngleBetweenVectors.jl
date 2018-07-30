e480beff41e51817a1b4608cf0b974f6cd001497
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-29 22:31:55 -0400

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -8,32 +8,6 @@ import LinearAlgebra: norm, dot
 
 const PI = Float64(pi)
 
-"""
-    angle2(point1::T, point2::T) where {T}
-
-Accurately ascertains the undirected angle (0 <= radians < pi)
-between two points given in N-dimensional Cartesian coordinates.
-
-Prefer this to `angle` for highest performance. Works best
-where all |coordinates| are in 2^±12 or [1/4_000,4_000].
-Use `angle2` where |coordinates| may be outside 2^±20 or
-[1/1_000_000,1_000_000]. 
-
-If one of the points is at the origin, the result is zero.
-
-You *must* define a tuple constructor `Tuple(x::T) = ...`.
-"""
-function angle2(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
-    point1a = point1 .* norm(point2)
-    point2a = point2 .* norm(point1)
-    
-    subpoints = point1a .- point2a
-    subpoints = dot(subpoints, subpoints)
-    addpoints = point1a .+ point2a
-    addpoints = dot(addpoints, addpoints)
-    
-    2 * atan(sqrt(subpoints ./ addpoints))
-end
 
 """
     angle(point1::T, point2::T) where {T}
@@ -60,6 +34,34 @@ function angle(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
     a = 2 * atan(norm(y) / norm(x))
    
     !signbit(a) && !(signbit(PI - a)) ? a : (signbit(a) ? zero(F) : PI)
+end
+
+
+"""
+    angle2(point1::T, point2::T) where {T}
+
+Accurately ascertains the undirected angle (0 <= radians < pi)
+between two points given in N-dimensional Cartesian coordinates.
+
+Prefer this to `angle` for highest performance. Works best
+where all |coordinates| are in 2^±12 or [1/4_000,4_000].
+Use `angle` where |coordinates| may be outside 2^±20 or
+[1/1_000_000,1_000_000]. 
+
+If one of the points is at the origin, the result is zero.
+
+You *must* define a tuple constructor `Tuple(x::T) = ...`.
+"""
+function angle2(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
+    point1a = point1 .* norm(point2)
+    point2a = point2 .* norm(point1)
+    
+    subpoints = point1a .- point2a
+    subpoints = dot(subpoints, subpoints)
+    addpoints = point1a .+ point2a
+    addpoints = dot(addpoints, addpoints)
+    
+    2 * atan(sqrt(subpoints ./ addpoints))
 end
 
 @inline angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
