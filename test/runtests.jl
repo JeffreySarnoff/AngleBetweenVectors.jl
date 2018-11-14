538cdabec07e45ec21214da215950d80ed9cf834
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-13 22:52:09 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,9 +12,9 @@ point3 = (-1.0, 0.0)
 @test angle(point1, point2) == pi/2.0
 @test angle(point1, point3) == pi/1.0
 
-point1 = (1, -1, 0,  0)
-point2 = (0,  1, 0, -1)
+point1 = (1.0, -1.0, 0.0,  0.0)
+point2 = (0.0,  1.0, 0.0, -1.0)
 
 # note: 2pi/3 !== Float64(2*BigFloat(pi)/3)
-@test angle(point1, point2) == Float64(2*BigFloat(pi)/3)
+@test angle(point1, point2) ≈ Float64(2*BigFloat(pi)/3)
 @test angle(point1, point2) == angle(point2, point1)
