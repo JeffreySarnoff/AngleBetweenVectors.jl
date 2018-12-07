852c26c75e4ebd7ae8ec9c8ff594666472d35f13
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-07 01:31:11 -0500

    remove commented out parts

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -9,21 +9,22 @@ import LinearAlgebra: norm, dot
 
 @inline unitvec(p) = p ./ norm(p)
 
-
 """
     angle(point1::T, point2::T) where {T}
 
 Accurately ascertains the undirected angle (0 <= radians < pi)
 between two points given in N-dimensional Cartesian coordinates.
 
-Prefer this to `fastangle` for highest accuracy.  Suggested
-when |coordinates| may be outside 2^±20 or [1/1_000_000,1_000_000].
-Strongly recommended when any |coordinates| are outside 2^±24 or
-[1/16_000_000,16_000_000].
+Prefer this to `acos` alternatives
+- more reliably accurate
+- more consistently stable
+
+Suggested when any |coordinate| of either point may be outside 2^±20 or [1/1_000_000, 1_000_000].
+Strongly recommended when any |coordinate| is outside 2^±24 or [1/16_000_000, 16_000_000].
 
 If one of the points is at the origin, the result is zero.
 
-You *must* define a tuple constructor `Tuple(x::T) = ...`.
+You *must* define a tuple constructor `Tuple(x::T) = ...` if one does not already exist.
 """
 function angle(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
     unitvec1 = unitvec(point1)
@@ -39,44 +40,5 @@ end
 
 @inline angle(point1::V, point2::V) where {T, V<:Vector{T}} = angle(Tuple(point1), Tuple(point2))
 @inline angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
-
-
-#=
-fastangle needs to be considerably faster
-"""
-    fastangle(point1::T, point2::T) where {T}
-
-Accurately ascertains the undirected angle (0 <= radians < pi)
-between two points given in N-dimensional Cartesian coordinates.
-
-Prefer `angle` to `fastangle` for highest accuracy.  Suggested
-when |coordinates| may be outside 2^±20 or [1/1_000_000,1_000_000].
-Strongly recommended when any |coordinates| are outside 2^±24 or
-[1/16_000_000,16_000_000].
-
-Prefer `fastangle` to `angle` for highest performance. Works best
-where all |coordinates| are in 2^±12 or [1/4_000,4_000].
-Use `angle` where |coordinates| may be outside 2^±20 or
-[1/1_000_000,1_000_000].
-
-If one of the points is at the origin, the result is zero.
-
-You *must* define a tuple constructor `Tuple(x::T) = ...`.
-"""
-function fastangle(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
-    point1a = point1 .* norm(point2)
-    point2a = point2 .* norm(point1)
-    
-    subpoints = point1a .- point2a
-    subpoints = dot(subpoints, subpoints)
-    addpoints = point1a .+ point2a
-    addpoints = dot(addpoints, addpoints)
-    
-    2 * atan(sqrt(subpoints ./ addpoints))
-end
-
-
-@inline fastangle(point1::T, point2::T) where {T} = fastangle(Tuple(point1), Tuple(point2))
-=#
 
 end # AngleBetweenVectors
