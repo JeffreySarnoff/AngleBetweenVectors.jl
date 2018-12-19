8768f34803e59562f7a00456504888f769199c4a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-19 06:20:10 -0500

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -5,7 +5,7 @@ import Base: angle
 import LinearAlgebra: norm
 
 
-@inline unitvec(p) = p ./ norm(p)
+@inline unitize(p) = p ./ norm(p)
 
 """
     angle(point1::T, point2::T) where {T}
@@ -22,21 +22,20 @@ Strongly recommended when any |coordinate| is outside 2^±24 or [1/16_000_000, 1
 
 If one of the points is at the origin, the result is zero.
 
-You *must* define a tuple constructor `Tuple(x::T) = ...` if one does not already exist.
+You *must* define a tuple constructor `Tuple(x::YourPointType) = ...` if one does not already exist.
 """
-function angle(point1::NT, point2::NT) where {N,T,NT<:NTuple{N,T}}
-    unitvec1 = unitvec(point1)
-    unitvec2 = unitvec(point2)
+function angle(point1::A, point2::A) where {N,T<:Real,NT<:NTuple{N,T}, V<:Vector{T}, A<:Union{NT,V}}
+    unitpoint1 = unitize(point1)
+    unitpoint2 = unitize(point2)
 
-    y = unitvec1 .- unitvec2
-    x = unitvec1 .+ unitvec2
+    y = unitpoint1 .- unitpoint2
+    x = unitpoint1 .+ unitpoint2
 
     a = 2 * atan(norm(y) / norm(x))
 
     !(signbit(a) || signbit(T(pi) - a)) ? a : (signbit(a) ? zero(T) : T(pi))
 end
 
-@inline angle(point1::V, point2::V) where {T, V<:Vector{T}} = angle(Tuple(point1), Tuple(point2))
 @inline angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
 
 end # AngleBetweenVectors
