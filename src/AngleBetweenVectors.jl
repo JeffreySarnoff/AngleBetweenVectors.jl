9b86b9466b123bb75199c9f40bc0d2d20a84b559
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-07-14 12:59:08 -0400

    Update AngleBetweenVectors.jl

--- src/AngleBetweenVectors.jl
+++ src/AngleBetweenVectors.jl
@@ -26,6 +26,18 @@ If one of the points is at the origin, the result is zero.
 You *must* define a tuple constructor `Tuple(x::YourPointType) = ...` if one does not already exist.
 """
 function angle(point1::P, point2::P) where {P<:Union{NTuple{N,T}, AbstractVector{T}}} where {N,T}
+    sumpt1sqr = foldl(+, pt1 .* pt1)
+    sumpt2sqr = foldl(+, pt2 .* pt2)
+    sqrtpt1sqr = sqrt(sumpt1sqr)
+    sqrtpt2sqr = sqrt(sumpt2sqr)
+    adplusbe = foldl(+, pt1 .* pt2)
+    adplusbe2 = adplusbe .* adplusbe
+    numer = adplusbe
+    denom = sqrtpt1sqr * sqrtpt2sqr * (1 + sqrt(1 - (adplusbe2 / (sumpt1sqr * sumpt2sqr))))
+    T(pi)*0.5 - atan(numer,denom)
+end
+
+#=
     unitpoint1 = unitize(point1)
     unitpoint2 = unitize(point2)
 
@@ -36,6 +48,7 @@ function angle(point1::P, point2::P) where {P<:Union{NTuple{N,T}, AbstractVector
 
     !(signbit(a) || signbit(float(T)(pi) - a)) ? a : (signbit(a) ? zero(T) : float(T)(pi))
 end
+=#
 
 angle(point1::T, point2::T) where {T} = angle(Tuple(point1), Tuple(point2))
 
