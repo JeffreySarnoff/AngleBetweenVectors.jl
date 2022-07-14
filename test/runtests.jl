01c91199fef4d4e8b9178c81662c98ea7f3868d6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-07-14 16:16:24 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,4 +21,13 @@ point2 = (0.0,  1.0, 0.0, -1.0)
 @test angle(point1, point2) == Float64(2*BigFloat(pi)/3)
 @test angle(point1, point2) == angle(point2, point1)
 
+struct P{T}
+    x::T
+    y::T
+end
+
+BaseTuple(p::P) = (p.x, p.y)
+
+@test angle(P(1.0,0.5), P(0.25, 1.5))
+
 end
